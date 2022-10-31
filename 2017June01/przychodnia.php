189f71a23cbd7d68a9aea0d4ae89f20580d2d8e4
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-10-31 14:07:11 +0100

    Add files via upload

---
+++ 2017June01/przychodnia.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Document</title>
+        <link rel="stylesheet" href="przychodnia.css"/>
+    </head>
+
+    <body>
+        <section id="baner">
+            <h1>PRAKTYKA LEKARZA RODZINNEGO</h1>
+        </section>
+
+        <section id="lewy">
+            <h3>LISTA PACJENTÓW</h3>
+            <?php
+                $conn = mysqli_connect('localhost','root','','przychodnia');
+
+                $sql = "SELECT `id`, `imie`, `nazwisko` FROM pacjenci;";
+                $result = mysqli_query($conn,$sql);
+
+                foreach($result as $value)
+                    echo($value['id']." ".$value['imie']." ".$value['nazwisko']."<br/>");
+
+                mysqli_close($conn);
+            ?>
+
+            <br/><br/>
+
+            <form method="POST" action="pacjent.php">
+                Podaj id:
+                <br/>
+
+                <input type="number" name="id"/>
+                <br/>
+
+                <button type="submit">Pokaż dane</button>
+            </form>
+
+            <h3>LEKARZE</h3>
+            <ul>
+                <li>pn - śr</li>
+                <ol>
+                    <li>Anna Kwiatkowska</li>
+                    <li>Jan Kowalewski</li>
+                </ol>
+                <li>czw - pt</li>
+                <ol>
+                    <li>Krzysztof Nowak</li>
+                </ol>
+            </ul>
+        </section>
+
+        <section id="prawy">
+            <h2>INFORMACJE SZCZEGÓŁOWE O PACJENCIE</h2>
+            <p>Brak wybranego pacjenta</p>
+        </section>
+
+        <section id="stopka">
+            <p>utworzone przez: ***********</p>
+            <a href="kwerendy.txt">Pobierz plik z kwerendami</a>
+        </section>
+    </body>
+</html>
